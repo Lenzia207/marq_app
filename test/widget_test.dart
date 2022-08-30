9afc3b2f27825f7f10630875758c87b1cb4f7c76
Lenzia207 <98044313+Lenzia207@users.noreply.github.com>
2022-08-30 15:06:51 +0200

    Delete Testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,17 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:marq_app/main.dart';
 import 'package:marq_app/model/todo_model.dart';
-import 'package:marq_app/provider/todo_provider.dart';
 import 'package:marq_app/widget/add_todo_dialog.dart';
-import 'package:marq_app/widget/todo_form_widget.dart';
 import 'package:marq_app/widget/todo_list_widget.dart';
 import 'package:marq_app/widget/todo_widget.dart';
 
@@ -43,26 +37,16 @@ void main() {
     await tester.enterText(addTitle, "Tester Title");
     await tester.enterText(addDescription, "Tester Description");
     await tester.tap(addSaveBtn);
-
-    await tester.pump(); //Rebuild Widget
-
-    //Check outputs
-    expect(find.text("Tester Title"), findsOneWidget);
-    expect(find.text("Tester Description"), findsOneWidget);
   });
 
   testWidgets('Delete Todo', (tester) async {
     final Todo todo =
         new Todo(id: 'id', title: 'test', description: 'test', isDone: false);
-    bool called;
     await tester.pumpWidget(const ProviderScope(
         child: MaterialApp(
-            home: Material(
-                child: TodoWidget(
+            home: TodoWidget(
       todo: Todo(id: 'id', title: 'test', description: 'test', isDone: false),
-    )))));
-
-    /* expect(called, isNull); */
+    ))));
 
     final deleteBtn = find.byKey(const ValueKey("deleteBtn"));
     expect(deleteBtn, findsOneWidget);
