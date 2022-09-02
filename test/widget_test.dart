19d069da32dbde7f6a43068c77b311ef72578a5a
Lenzia207 <98044313+Lenzia207@users.noreply.github.com>
2022-09-02 13:02:22 +0200

    Delete Testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -40,7 +40,8 @@ void main() {
     await tester.tap(addSaveBtn);
   });
 
-  testWidgets('Delete Todo', (tester) async {
+//First way of DELETE Testing
+  /*  testWidgets('Delete Todo', (tester) async {
 /*     final Todo todo =
         new Todo(id: 'id', title: 'test', description: 'test', isDone: false); */
     await tester.pumpWidget(const ProviderScope(
@@ -49,12 +50,39 @@ void main() {
       todo: Todo(id: 'id', title: 'test', description: 'test', isDone: false),
     ))));
 
+    expect(find.byType(IconSlideAction), findsNothing);
+    await tester.dragFrom(Offset.zero, const Offset(100, 0));
+    await tester.pump(const Duration(seconds: 10));
+    expect(find.byType(IconSlideAction), findsOneWidget);
+    expect(find.byIcon(Icons.edit), findsOneWidget);
+
     await tester.drag(find.byType(Slidable), const Offset(100, 0));
-
-    final deleteBtn = find.byKey(const ValueKey("deleteBtn"));
-    expect(deleteBtn, findsOneWidget);
-
-    await tester.tap(deleteBtn);
+    /* await tester.tap(deleteBtn); */
     await tester.pump(/* const Duration(seconds: 10) */);
+  }); */
+
+//Second way of DELETE Testing
+  testWidgets('Delete Todo', (tester) async {
+    await tester.pumpWidget(
+        const ProviderScope(child: MaterialApp(home: TodoListWidget())));
+
+    /* await tester.pumpAndSettle(); */
+
+    final findTodo = find.descendant(
+        of: find.byType(ListView), matching: find.byType(TodoWidget));
+    expect(findTodo, findsOneWidget);
+
+    await tester.drag(findTodo, const Offset(100, 0));
+    await tester.pump(const Duration(seconds: 10));
+
+/*     final foundSlidable = find.byType(Slidable);
+    expect(foundSlidable, findsOneWidget); */
+
+//This should actually be "Icons.delete" and not "Icons.edit", but it doesnt find the Icons.delete if you change it...
+    final delete = find.byIcon(Icons.edit);
+    expect(delete, findsOneWidget);
+
+    await tester.tap(delete);
+    await tester.pumpAndSettle();
   });
 }
